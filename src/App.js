3b07ed7812d86527d33586bf96e5d73ca537156e
Wallyson <wallysom2@gmail.com>
2022-07-15 10:55:13 -0300

    componentizando posts

--- src/App.js
+++ src/App.js
@@ -1,40 +1,25 @@
-import Comment from "./components/Comment"
+import React from "react"
+import Stories from "./components/Stories"
+import Navbar from "./components/Navbar";
 
-function App()
-    {
-    return (   
-    <>
-    <h1>Hello World</h1>
-    <Comment titulo="first" assunto= "primeiro ass" texto = "texto1 texto1texto1texto1"/>
-    <Comment titulo="segundo" assunto= "segundo ass" texto = "segundo texto1texto1texto1"/>
-    </>
+function App() {
+    return (
+        <div class="root">
+            <div class="corpo">
+                <div class="esquerda">
+                </div>
+            </div>
+            <div class="fundo-mobile">
+                <ion-icon name="home"></ion-icon>
+                <ion-icon name="search-outline"></ion-icon>
+                <ion-icon name="add-circle-outline"></ion-icon>
+                <ion-icon name="heart-outline"></ion-icon>
+                <ion-icon name="person-outline"></ion-icon>
+            </div>
+            <Navbar />
+            <Stories />
+        </div>
     )
-    } 
+}
 
-//Create a function called sumInternalProductor(arr), which receives an array of arrays, then the function must sum the product of multiplying the elements of the internal arrays.  Ex arr = [ [5], [12, 3, 0], [2, 3, 5] ] => Should return 35
-function sumInternalProductor(arr)
-    {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++)
-        {
-        for(let j = 0; j < arr[i].length; j++)
-            {
-            sum += arr[i][j]
-            }
-        }
-    return sum
-    }
-
-
-
-
-
-
-
- 
-
-
-
-
-  
-  export default App;
+export default App;
---
+++ src/components/Stories.jsx
@@ -0,0 +1,31 @@
+const dados = [
+  { img: "./assets/img/9gag.svg", nome: "9gag" },
+  { img: "./assets/img/barked.svg", nome: "barked" },
+  { img: "./assets/img/bad.vibes.memes.svg", nome: "bad.vibes.memes" },
+  { img: "assets/img/adorable_animals.svg", nome: "adorable_animals" },
+  { img: "assets/img/chibirdart.svg", nome: "chibirdart" },
+  { img: "assets/img/razoesparaacreditar.svg", nome: "razoesparaacreditar" },
+  { img: "assets/img/smallcutecats.svg", nome: "smallcutecats" },
+  { img: "assets/img/gato-telefone.svg", nome: "gato-telefone" },
+]
+
+function Stories() {
+  return (
+    <div class="stories">
+      {dados.map(dado => (
+        <div class="story">
+          <div class="imagem">
+            <img src={dado.img} /></div>
+          <div class="usuario">  {dado.nome}  </div>
+        </div>
+      ))}
+
+      <div class="setinha">
+        <ion-icon name="chevron-forward-circle"></ion-icon>
+      </div>
+
+    </div>
+  )
+}
+
+export default Stories
